b45773d0eee7923b0bbf0b7a3809d89db3734ceb
Patryk Golabek <patryk@translucentcomputing.com>
2016-10-22 11:12:40 -0400

    updated main js name

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     files: [
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
-      'angular-cache-buster.js',
+      'tc-angular-cache-buster.js',
       'test/*.js'
     ],
 
